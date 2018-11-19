9419761c9a753f87240e7ceef51b4683ff7f9eba
huae <huanghe2r@foxmail.com>
2018-11-19 23:07:12 +0800

    创建项目目录结构

---
+++ src/com/huae/coolweather/dao/CoolWeatherOpenHelper.java
@@ -0,0 +1,32 @@
+package com.huae.coolweather.dao;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * CoolWeather数据库访问类
+ * @author huang
+ *
+ */
+public class CoolWeatherOpenHelper extends SQLiteOpenHelper{
+
+	public CoolWeatherOpenHelper(Context context, String name,
+			CursorFactory factory, int version) {
+		super(context, name, factory, version);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		// TODO Auto-generated method stub
+		
+	}
+}
